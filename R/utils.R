a55446d4350fb6affade601b8cde40dae0eb473f
Brandon Alejandro Mosqueda González <brandon16mg@gmail.com>
2020-10-11 13:03:35 -0500

    * ZAP random forest model training and predict functions * utils files with cholesky function and other auxiliar function * New functions added to export section in namespace

---
+++ R/utils.R
@@ -0,0 +1,135 @@
+is_number <- function(value) {
+  if (is.factor(value)) {
+    return(FALSE)
+  }
+
+  suppressWarnings(value <- as.numeric(value))
+
+  return(!is.na(value))
+}
+
+is_int <- function(number) {
+  return(is_number(number) && as.numeric(number) %% 1 == 0)
+}
+
+is_discrete <- function(number) {
+  if (!all(is_int(number))) {
+    return(FALSE)
+  }
+
+  return(all(number >= 0))
+}
+
+get_formula <- function(response, predictors) {
+  return(formula(paste0(response, " ~ ",
+                        paste0(predictors,  collapse = " + "))))
+}
+
+#' @title Cholesky
+#'
+#' @description Compute the Cholesky factorization of a non-real symmetric
+#'              positive-definite square matrix.
+#'
+#' @param G (\code{numeric - matrix}) an object to apply this method, it could
+#'        be non positive-definite matrices.
+#' @param tolerance (\code{double}) Tolerance level, by default is 1e-10.
+#'
+#' @export
+cholesky <- function(G, tolerance = 1e-10) {
+  data_names <- colnames(G)
+
+  tryCatch({
+    result <- t(chol(G))
+  }, error=function(error_condition) {
+      G <- (G + t(G)) / 2
+      EigenA <- eigen(G)
+      d_A    <- EigenA$values
+      V_A    <- EigenA$vectors
+      d_A[which(d_A < tolerance)] <- tolerance*100L
+      pos_A1 <- which(d_A > tolerance)
+      if (identical(pos_A1, integer(0))) {
+        pos_A <- 1L
+      } else {
+        pos_A <- pos_A1
+      }
+      d_A_Star <- d_A[pos_A]
+      V_A_Star <- V_A[, pos_A]
+
+      if (length(pos_A) == 1L) {
+        d_A_Star <- 1L / d_A_Star
+        LG <- d_A_Star * sqrt(V_A_Star)
+      } else {
+        d_A_Star <- diag(d_A_Star)
+        LG <- (V_A_Star %*% sqrt(d_A_Star))
+      }
+
+      result <<- LG
+  })
+
+  rownames(result) <- data_names
+  colnames(result) <- data_names
+
+  return(result)
+}
+
+.prepare_X_with_markers <- function(Pheno, Geno, with_interaction, is_uni_env) {
+  ZG <- model.matrix(~0 + as.factor(Pheno$Line))
+  ZG1 <- ZG %*% Geno
+  X <- ZG1
+
+  # It's important to respect the order: Env, Lines and Interaction effect
+  if (!is_uni_env) {
+    #####of environment and GxE###################
+    Z.E <- model.matrix(~0 + as.factor(Pheno$Env))
+    X <- cbind(Z.E, X)
+
+    if (with_interaction) {
+      ####Linear kernel for the GE term
+      Z.GE <- model.matrix(~0 + ZG1:as.factor(Pheno$Env))
+      X <- cbind(X, Z.GE)
+    }
+  }
+
+  return(X)
+}
+
+.prepare_X_without_markers <- function(Pheno, Geno, with_interaction,
+                                       is_uni_env) {
+  # Of lines
+  ZG <- model.matrix(~0 + as.factor(Pheno$Line))
+
+  # Creating the linear kernel of lines for RKHS
+  ZL <- get_cholesky(Geno)
+  ZG1 <- ZG %*% ZL
+  X <- ZG1
+
+  # It's important to respect the order: Env, Lines and Interaction effect
+  if (!is_uni_env) {
+    # Of environment
+    Z.E <- model.matrix(~0 + as.factor(Pheno$Env))
+    X <- cbind(Z.E, X)
+
+    if (with_interaction) {
+      # GxE
+      Z.GE <- model.matrix(~0 + ZG1:as.factor(Pheno$Env))
+      X <- cbind(X, Z.GE)
+    }
+  }
+
+  return(X)
+}
+
+.prepare_X <- function(Pheno, Geno, with_markers, with_interaction,
+                       is_uni_env) {
+  if (with_markers) {
+    X <- .prepare_X_with_markers(Pheno, Geno, with_interaction, is_uni_env)
+  } else {
+    X <- .prepare_X_without_markers(Pheno, Geno, with_interaction, is_uni_env)
+  }
+
+  X <- as.matrix(X)
+  colnames(X) <- make.names(colnames(X))
+  rownames(X) <- make.names(rownames(X))
+
+  return(X)
+}
--- R/zap.rfsrc.R
+++ R/zap.rfsrc.R
@@ -1,7 +1,100 @@
-zap.rfsrc <- function() {
-  g <- function(x, y) x / (1 - exp(-x)) - y
+g <- function(x, y) x / (1 - exp(-x)) - y
+
+zap.rfsrc <- function(X, y,
+                      ntree_theta = 500,
+                      mtry_theta = ncol(X) / 3,
+                      nodesize_theta = 5,
+                      ntree_lambda = 500,
+                      mtry_lambda = ncol(X) / 3,
+                      nodesize_lambda = 5,
+                      importance = FALSE) {
+  if (!all(is_discrete(y))) {
+    stop("ZAP random forest only can be used with discrete data")
+  } else if (nrow(X) != length(y)) {
+    stop("X must have the same number of rows that elements in y")
+  }
+
+  zeros_percentaje <- round(sum(y == 0) / length(y) * 100, 1)
+
+  if (zeros_percentaje < 20) {
+    warning("It looks that your data has only ", zeros_percentaje, " percentaje ",
+            "of zeros, in this case maybe ZAP random forest could not be your ",
+            "best option.")
+  }
+
+  Data <- as.data.frame(X)
+  predictors <- colnames(Data)
+  Data$y <- y
+  Data$y0 <- Data$y == 0
+
+  # Binary random forest
+  theta_forest <- rfsrc(
+    get_formula("y0", predictors),
+    Data,
+    ntree = ntree_theta,
+    mtry = mtry_theta,
+    nodesize = nodesize_theta,
+    importance = importance
+  )
+
+  # For lambda include only the observations with values greater than zero.
+  Data <- Data[Data$y > 0, ]
+
+  lambda_forest <- rfsrc(
+    get_formula("y", predictors),
+    Data,
+    ntree = ntree_lambda,
+    mtry = mtry_lambda,
+    nodesize = nodesize_lambda,
+    splitrule = "custom1",
+    importance = importance
+  )
+
+  model <- structure(list(theta_forest = theta_forest,
+                          lambda_forest = lambda_forest),
+                     class = "zap.rfsrc")
+
+  return(model)
 }
 
-brandon <- function() {
-  print("Brandon function from zap.rfsrc")
+predict_theta <- function(theta_forest, X) {
+  return(predict(theta_forest, newdata=X)$predicted)
+}
+
+predict_lambda <- function(lambda_forest, X) {
+  yhat0 <- predict(lambda_forest, newdata=X)$predicted
+  N <- nrow(X)
+
+  lambda_predictions <- rep(0, N)
+
+  for(i in 1:N) {
+    lambda_predictions[i] <- uniroot(g, interval=c(-1, yhat0[i] + 2),
+                                     y=yhat0[i])$root
+    lambda_predictions[i] <- lambda_predictions[i] *
+                             (lambda_predictions[i] > 0)
+  }
+
+  return(lambda_predictions)
+}
+
+predict.zap.rfsrc <- function(model, X, type="original") {
+  thetas <- predict_theta(model$theta_forest, X)
+  lambdas <- predict_lambda(model$lambda_forest, X)
+  N <- nrow(X)
+
+  if (type == "original") {
+    predictions <- ((1 - thetas) * lambdas) / (1 - exp(-lambdas))
+  } else if (type == "custom") {
+    predictions <- rep(0, N)
+
+    no_zero_predictions_indices <- thetas < 0.5
+    predictions[no_zero_predictions_indices] <-
+      lambdas[no_zero_predictions_indices]
+  } else {
+    stop("type parameter only can be original or custom, not", type)
+  }
+
+  return(list(predicted = round(predictions),
+              lambda = round(lambdas),
+              theta = round(thetas)))
 }
